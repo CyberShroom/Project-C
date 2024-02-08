009d2284776803cf5866f5b9e29dc04501d9e9cd
CyberShroom <38116754+Skyx7x@users.noreply.github.com>
2024-02-07 21:14:59 -0600

    Moved Repository Into Project
    It created its own folder inside the project for some reason.

---
+++ Source/ToTheCenter/ToTheCenter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ToTheCenter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToTheCenter, "ToTheCenter" );
