77a6c8bcf4747a88add1e3e74ddcebc62dccccf6
CyberShroom <cmorganx7x@gmail.com>
2024-05-24 21:34:31 -0500

    Removed C++
    I decided to cut out all c++. Its not as complicated as it first appears, but there are some things that 100% need more research.

--- Source/ToTheCenter_0/ToTheCenter_0.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ToTheCenter_0.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToTheCenter_0, "ToTheCenter_0" );
