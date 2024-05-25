c3e18ea19a20cf6aab58db7055fa17489f306128
CyberShroom <cmorganx7x@gmail.com>
2024-05-24 19:08:56 -0500

    Removed Starter Content And Dabbled In C++
    Starter content folder removed. Started working with c++ and promptly deleted all of it. Corrupted original files so had to make a new project but all the content is fine. Made some updates to the ui.

---
+++ Source/ToTheCenter_0/ToTheCenter_0.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ToTheCenter_0.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToTheCenter_0, "ToTheCenter_0" );
